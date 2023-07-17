2563136df80da0c59a49508f025da3d8538158f7
TigerBaseball <121096971+TigerBaseball@users.noreply.github.com>
2023-07-17 15:34:41 +0900

    First commit

--- src/variables.js
+++ src/variables.js
@@ -1,11 +1,28 @@
-//variables for list_sheet_info();
-const main_sheet_name = "display-sheet-info";
+//variables for all instances
+const notionToken = 'secret_nu3LsoywDpLmwxhqKBP3sEwJAIQTfFED7egtspiWouS';
+const sheetNameChart = "Chart";
 
-//variables for display_file_info();
-const display_sheet = "display-file-info";
+//variables for instance to create a pie chart for database of my spending
+const tableId_1 = '7b7b05948da74c7684502130d174396f';
+const sheetName_1 = 'My_Spending';
+const item_1 = 'Item';
+const category_1 = 'Category';
+const monthlyPay_1 = 'Monthly_Pay';
+const chartName_1 = '支出テーブル(自分)';
 
-//variables for add_editors_no_notification();
-const add_editor_sheet = "add-editors-no-notification";
 
-//variables for add_editors();
-const add_editor_sheet2 = "add-editors";
+//variables for instance to create a pie chart for database of family spending
+const tableId_2 = 'c30d354e96c54ee8a1895cd26d9c88ea';
+const sheetName_2 = 'Family_Spending';
+const item_2 = '項目';
+const category_2 = 'カテゴリー';
+const pay_2 = '支払金額';
+const chartName_2 = '支出テーブル(家族)';
+
+//variables for instance to create a pie chart for database of assetmanagement
+const tableId_3 = 'f68c67cdae484a70b4018e766e097482';
+const sheetName_3 = 'Asset_Management';
+const item_3 = '項目';
+const category_3 = 'カテゴリー';
+const pay_3 = '投資額';
+const chartName_3 = '資産運用テーブル';
